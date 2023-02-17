5dd2b9a97b9753ede4b0b69cc6ec7be7ab691433
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-02-17 06:06:52 +0000

    Category App partial set-up

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'api',
+    'api.category',
 ]
 
 MIDDLEWARE = [
